72d12f3cca647520b66ad3dc5b08558acc204a8d
Tan Yanbo <tanyanbo95@hotmail.com>
2023-08-07 21:00:41 +0800

    refactor: change values in decode_table to type u8

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,5 @@
 #![feature(test)]
 
-const ENCODE_TABLE: [&str; 64] = [
-    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
-    "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
-    "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4",
-    "5", "6", "7", "8", "9", "+", "/",
-];
-
-const DECODE_TABLE: [i8; 128] = [
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63,
-    52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, 64, -1, -1, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8,
-    9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1, 26,
-    27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
-    51, -1, -1, -1, -1, -1,
-];
-
 pub fn encode(bytes: Vec<u8>) -> String {
     let remainder = bytes.len() % 3;
 
@@ -70,7 +54,7 @@ fn decode(input: String) -> Vec<u8> {
         let first_sextet = DECODE_TABLE[input_bytes[input_bytes.len() - 4] as usize];
         let second_sextet = DECODE_TABLE[input_bytes[input_bytes.len() - 3] as usize];
 
-        first_extra_value = Some(((first_sextet << 2) | ((second_sextet & 0b110000) >> 4)) as u8);
+        first_extra_value = Some((first_sextet << 2) | ((second_sextet & 0b110000) >> 4));
 
         input_bytes.truncate(input_bytes.len() - 4);
     } else if input_bytes[input_bytes.len() - 1] == 61 {
@@ -78,9 +62,9 @@ fn decode(input: String) -> Vec<u8> {
         let second_sextet = DECODE_TABLE[input_bytes[input_bytes.len() - 3] as usize];
         let third_sextet = DECODE_TABLE[input_bytes[input_bytes.len() - 2] as usize];
 
-        first_extra_value = Some(((first_sextet << 2) | ((second_sextet & 0b110000) >> 4)) as u8);
+        first_extra_value = Some((first_sextet << 2) | ((second_sextet & 0b110000) >> 4));
         second_extra_value =
-            Some((((second_sextet & 0b1111) << 4) | ((third_sextet & 0b111100) >> 2)) as u8);
+            Some(((second_sextet & 0b1111) << 4) | ((third_sextet & 0b111100) >> 2));
 
         input_bytes.truncate(input_bytes.len() - 4);
     }
@@ -91,7 +75,11 @@ fn decode(input: String) -> Vec<u8> {
         let third_sextet = DECODE_TABLE[input_bytes[i * 4 + 2] as usize];
         let fourth_sextet = DECODE_TABLE[input_bytes[i * 4 + 3] as usize];
 
-        if first_sextet == -1 || second_sextet == -1 || third_sextet == -1 || fourth_sextet == -1 {
+        if first_sextet == INVALID_INDEX
+            || second_sextet == INVALID_INDEX
+            || third_sextet == INVALID_INDEX
+            || fourth_sextet == INVALID_INDEX
+        {
             panic!("Invalid base64 string");
         }
 
@@ -110,6 +98,146 @@ fn decode(input: String) -> Vec<u8> {
 
     result
 }
+
+const ENCODE_TABLE: [&str; 64] = [
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
+    "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+    "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4",
+    "5", "6", "7", "8", "9", "+", "/",
+];
+
+const INVALID_INDEX: u8 = 100;
+
+const DECODE_TABLE: [u8; 128] = [
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    62,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    63,
+    52,
+    53,
+    54,
+    55,
+    56,
+    57,
+    58,
+    59,
+    60,
+    61,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    64,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    26,
+    27,
+    28,
+    29,
+    30,
+    31,
+    32,
+    33,
+    34,
+    35,
+    36,
+    37,
+    38,
+    39,
+    40,
+    41,
+    42,
+    43,
+    44,
+    45,
+    46,
+    47,
+    48,
+    49,
+    50,
+    51,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+    INVALID_INDEX,
+];
 
 #[cfg(test)]
 mod tests {
