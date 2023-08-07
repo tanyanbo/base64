88187f788fda213d6d25d85aa32cd2ed341e17a7
Tan Yanbo <tanyanbo95@hotmail.com>
2023-08-07 15:54:21 +0800

    feat: add base64 encode

---
+++ src/lib.rs
@@ -0,0 +1,51 @@
+const TABLE: [&str; 64] = [
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
+    "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+    "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4",
+    "5", "6", "7", "8", "9", "+", "/",
+];
+
+pub fn string_to_base64(input: String) -> String {
+    let bytes = input.into_bytes();
+    let mut res = String::from("");
+
+    for i in 0..(bytes.len() / 3) {
+        res += TABLE[((bytes[3 * i] & 0b11111100) >> 2) as usize];
+        res +=
+            TABLE[(((bytes[3 * i] & 0b11) << 4) | ((bytes[3 * i + 1] & 0b11110000) >> 4)) as usize];
+        res += TABLE[(((bytes[3 * i + 1] & 0b1111) << 2) | ((bytes[3 * i + 2] & 0b11000000) >> 6))
+            as usize];
+        res += TABLE[(bytes[3 * i + 2] & 0b111111) as usize];
+    }
+
+    let remainder = bytes.len() % 3;
+    match remainder {
+        0 => return res,
+        1 => {
+            res += TABLE[((bytes[bytes.len() - 1] & 0b11111100) >> 2) as usize];
+            res += TABLE[((bytes[bytes.len() - 1] & 0b11) << 4) as usize];
+            res += "==";
+            return res;
+        }
+        2 => {
+            res += TABLE[((bytes[bytes.len() - 2] & 0b11111100) >> 2) as usize];
+            res += TABLE[(((bytes[bytes.len() - 2] & 0b11) << 4)
+                | ((bytes[bytes.len() - 1] & 0b11110000) >> 4)) as usize];
+            res += TABLE[((bytes[bytes.len() - 1] & 0b1111) << 2) as usize];
+            res += "=";
+            return res;
+        }
+        _ => panic!("Invalid input"),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let res = string_to_base64("manlibrs".into());
+        println!("{}", res);
+    }
+}
